ab05273b294684c4a57c7ac038730445d475b55f
Ashirasan <fluk290344@gmail.com>
2024-12-09 12:42:49 +0700

    assignment 3

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             title: const Text(
-              'APP BAR',
+              'POP CAT',
               style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
             ),
             leading: IconButton(
               onPressed: () {},
               icon: const Icon(Icons.home),
             ),
-            actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.airplanemode_inactive_sharp)),IconButton(onPressed: (){}, icon:const Icon(Icons.wifi_lock_rounded))],
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.airplanemode_inactive_sharp)),
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.wifi_lock_rounded))
+            ],
           ),
           body: Column(
             children: [
-              const Text(
-                'POP CAT',
-                style: TextStyle(fontSize: 20),
-              ),
+              // const Text(
+              //   'POP CAT',
+              //   style: TextStyle(fontSize: 20),
+              // ),
               // const Text(
               //   'Boontaeng',
               //   style: TextStyle(fontSize: 20),
@@ -41,16 +47,87 @@ class MyApp extends StatelessWidget {
               //Image.network('https://cdn.britannica.com/41/126641-050-E1CA0E61/cat-suns-hill-Parthenon-Athens-Greece-Acropolis.jpg'),
               // Image.asset('assets/images/cat.jpg'),
               Image.asset('assets/images/pop-cat.gif'),
-              SizedBox(height: 15,),
-              ElevatedButton(
-                  onPressed: () {
-                    print("Click me ");
-                  },
-                  style: ButtonStyle(),
-                  child: const Text(
-                    'click Me',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text('ขอบคุณที่ใช้บริการ',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              const Text('เรายินดีที่ได้ร่วมเป็นส่วนหนึ่งในการเดินทางของคุณ',
+                  style: TextStyle(
+                    fontSize: 15,
                   )),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                children: [
+                  Text(
+                    'สรุปรายละเอียดการชาร์จ',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.calendar_month),
+                          const Text('วันที่ชาร์จ')
+                        ],
+                      ),
+                      const Text('9 กันยายน 2566'),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.ev_station),
+                          const Text('สถาณีชาร์จ')
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text("PEA"),
+                          Text('1212#1'),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              )
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     const Icon(Icons.heart_broken),
+              //     ElevatedButton(
+              //         onPressed: () {
+              //           print("Click me ");
+              //         },
+              //         style: ButtonStyle(),
+              //         child: const Text(
+              //           'click Me',
+              //           style: TextStyle(
+              //               fontSize: 20, fontWeight: FontWeight.bold),
+              //         )),
+              //   ],
+              // )
             ],
           ),
           floatingActionButton: FloatingActionButton(

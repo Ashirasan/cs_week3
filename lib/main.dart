8e684ece3613ddfa9da68653856c0104b01d6e74
Ashira <fluk290344@gmail.com>
2024-12-16 12:36:01 +0700

    week4 ev charging

--- lib/main.dart
+++ lib/main.dart
@@ -17,125 +17,182 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
           useMaterial3: true),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'POP CAT',
-              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-            ),
-            leading: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.home),
-            ),
-            actions: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.airplanemode_inactive_sharp)),
-              IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.wifi_lock_rounded))
-            ],
+        appBar: AppBar(
+          title: const Text(
+            'EV Application',
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
           ),
-          body: Column(
-            children: [
-              // const Text(
-              //   'POP CAT',
-              //   style: TextStyle(fontSize: 20),
-              // ),
-              // const Text(
-              //   'Boontaeng',
-              //   style: TextStyle(fontSize: 20),
-              // ),
-              //Image.network('https://cdn.britannica.com/41/126641-050-E1CA0E61/cat-suns-hill-Parthenon-Athens-Greece-Acropolis.jpg'),
-              // Image.asset('assets/images/cat.jpg'),
-              Image.asset('assets/images/pop-cat.gif'),
-              const SizedBox(
-                height: 15,
-              ),
-              const Text('ขอบคุณที่ใช้บริการ',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              const Text('เรายินดีที่ได้ร่วมเป็นส่วนหนึ่งในการเดินทางของคุณ',
-                  style: TextStyle(
-                    fontSize: 15,
-                  )),
-              const SizedBox(
-                height: 15,
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'สรุปรายละเอียดการชาร์จ',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home),
+          ),
+          // actions: [
+          //   IconButton(
+          //       onPressed: () {},
+          //       icon: const Icon(Icons.airplanemode_inactive_sharp)),
+          //   IconButton(
+          //       onPressed: () {}, icon: const Icon(Icons.wifi_lock_rounded))
+          // ],
+        ),
+        body: Column(
+          children: [
+            // const Text(
+            //   'POP CAT',
+            //   style: TextStyle(fontSize: 20),
+            // ),
+            // const Text(
+            //   'Boontaeng',
+            //   style: TextStyle(fontSize: 20),
+            // ),
+            //Image.network('https://cdn.britannica.com/41/126641-050-E1CA0E61/cat-suns-hill-Parthenon-Athens-Greece-Acropolis.jpg'),
+            Image.asset('assets/images/cat.jpg'),
+            // Image.asset('assets/images/pop-cat.gif'),
+            const SizedBox(
+              height: 15,
+            ),
+            const Row(
+              children: [
+                Text(
+                  'Battery Details',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: const Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Icon(Icons.battery_0_bar),
+                            Expanded(child: Text('Battery Capacity (kWh)')),
+                            Expanded(child: SizedBox(height: 45,child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),),),))
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Icon(Icons.bolt),
+                            Expanded(child: Text('Efficiency')),
+                            Expanded(child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),),))
+                          ],
+                        ),
+                      ],
+                    )),
+                const SizedBox(height: 10,),
+                const Row(
+                  children: [
+                    Text(
+                      'Charging Details',
+                      textAlign: TextAlign.left,
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: const Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Icon(Icons.battery_0_bar),
+                            Expanded(child: Text('Voltage(V)')),
+                            Text('225'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Icon(Icons.bolt),
+                            Expanded(child: Text('Charging Power(kWh)')),
+                            Text('0.84'),
+                          ],
+                        ),
+                      ],
+                    )),
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.calendar_month),
-                          const Text('วันที่ชาร์จ')
-                        ],
+                      Container(
+                        width: 180,
+                        child: const TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Current SOC%'),
+                        ),
                       ),
-                      const Text('9 กันยายน 2566'),
+                      Container(
+                        width: 180,
+                        child: const TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Target SOC%'),
+                        ),
+                      ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Charge rate A'),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.ev_station),
-                          const Text('สถาณีชาร์จ')
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text("PEA"),
-                          Text('1212#1'),
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              )
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     const Icon(Icons.heart_broken),
-              //     ElevatedButton(
-              //         onPressed: () {
-              //           print("Click me ");
-              //         },
-              //         style: ButtonStyle(),
-              //         child: const Text(
-              //           'click Me',
-              //           style: TextStyle(
-              //               fontSize: 20, fontWeight: FontWeight.bold),
-              //         )),
-              //   ],
-              // )
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              debugPrint('FAB pressed');
-            },
-            child: const Icon(Icons.access_alarm),
-          )),
+                )
+              ],
+            ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     const Icon(Icons.heart_broken),
+            //     ElevatedButton(
+            //         onPressed: () {
+            //           print("Click me ");
+            //         },
+            //         style: ButtonStyle(),
+            //         child: const Text(
+            //           'click Me',
+            //           style: TextStyle(
+            //               fontSize: 20, fontWeight: FontWeight.bold),
+            //         )),
+            //   ],
+            // )
+            // const TextField(
+            //   decoration: InputDecoration(
+            //     border: OutlineInputBorder(),
+            //     labelText: 'Name',
+            //     hintText: 'Enter name',
+            //   ),
+            //   autofocus: true,
+            // ),
+          ],
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     debugPrint('FAB pressed');
+        //   },
+        //   child: const Icon(Icons.add_comment),
+        // )
+      ),
     );
   }
 }
